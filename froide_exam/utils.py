e15d389aec85cba3642066503f67fe5bea9227d1
krmax44 <hi@krmax44.de>
2022-05-18 10:22:30 +0200

    🏗️ only allow request for the last 6 years

--- froide_exam/utils.py
+++ froide_exam/utils.py
@@ -5,12 +5,12 @@ from django.urls import reverse
 
 
 TODAY = timezone.now().date()
-MIN_YEAR = 2010
 # max year is this year starting in July, otherwise year before
 MAX_YEAR = TODAY.year if TODAY.month >= 7 else TODAY.year - 1
+MIN_YEAR = MAX_YEAR - 6
 YEARS = list(range(MIN_YEAR, MAX_YEAR + 1))
 
-REFERENCE_NAMESPACE = 'examvsp:'
+REFERENCE_NAMESPACE = "examvsp:"
 
 # waiting requests with no response in 3 months are stale
 # and should be requested by another user again
@@ -18,9 +18,9 @@ STALE_THRESHOLD = 90
 
 
 class SubjectYear(object):
-
-    def __init__(self, user=None, subject=None, year=None, same_requests=None,
-                 state=None):
+    def __init__(
+        self, user=None, subject=None, year=None, same_requests=None, state=None
+    ):
         self.subject = subject
         self.year = year
         self.user = user
@@ -28,14 +28,14 @@ class SubjectYear(object):
         self.state = state
 
     def __str__(self):
-        return '{}: {}'.format(self.subject, self.year)
+        return "{}: {}".format(self.subject, self.year)
 
     def get_reference(self, curriculum):
-        return '{namespace}{curriculum_id}:{subject_id}:{year}'.format(
+        return "{namespace}{curriculum_id}:{subject_id}:{year}".format(
             namespace=REFERENCE_NAMESPACE,
             curriculum_id=curriculum.id,
             subject_id=self.subject.id,
-            year=self.year
+            year=self.year,
         )
 
     @property
@@ -59,7 +59,7 @@ class SubjectYear(object):
                     if self.user.id == foirequest.user_id:
                         by_user = request
 
-                    if foirequest.resolution == 'successful':
+                    if foirequest.resolution == "successful":
                         with_success = request
 
                 if request.url:
@@ -113,7 +113,7 @@ class SubjectYear(object):
         if not self.exam_request.foirequest:
             return
         fr = self.get_same_request() or self.exam_request.foirequest
-        return 'successful' not in fr.resolution
+        return "successful" not in fr.resolution
 
     def can_request(self):
         if not self.state and not self.state.needs_request():
@@ -122,8 +122,10 @@ class SubjectYear(object):
             return False
         if not self.exam_foirequest:
             return True
-        if (self.state.legal_status == 'request_not_publish' and
-                not self.user_requested()):
+        if (
+            self.state.legal_status == "request_not_publish"
+            and not self.user_requested()
+        ):
             return True
         if not self.user.is_authenticated:
             return True
@@ -142,96 +144,93 @@ class SubjectYear(object):
 
     def user_requested(self):
         return (
-            self.exam_request and
-            self.exam_request.foirequest and
-            self.user == self.exam_request.foirequest.user
+            self.exam_request
+            and self.exam_request.foirequest
+            and self.user == self.exam_request.foirequest.user
         )
 
     def is_one_click(self):
-        if self.state.legal_status != 'request_not_publish':
+        if self.state.legal_status != "request_not_publish":
             return False
         return self.exam_foirequest
 
     def make_request_url(self):
         if not self.curricula:
             return
-        if hasattr(self, '_request_url'):
+        if hasattr(self, "_request_url"):
             return self._request_url
         curriculum = self.curricula[0]
 
         pb_slug = self.state.publicbody.slug
-        url = reverse('foirequest-make_request', kwargs={
-            'publicbody_slug': pb_slug
-        })
+        url = reverse("foirequest-make_request", kwargs={"publicbody_slug": pb_slug})
         name = curriculum.name
-        subject = ('{name}-Aufgaben im Fach {subject} '
-                   'im Jahr {year} in {state}'.format(
-                       name=name,
-                       subject=self.subject,
-                       year=self.year,
-                       state=curriculum.state.name,
-                   ))
+        subject = (
+            "{name}-Aufgaben im Fach {subject} "
+            "im Jahr {year} in {state}".format(
+                name=name,
+                subject=self.subject,
+                year=self.year,
+                state=curriculum.state.name,
+            )
+        )
         if len(subject) > 250:
-            subject = subject[:250] + '...'
+            subject = subject[:250] + "..."
         body = (
-            'Die Aufgaben, Erwartungshorizonte und Lösungen für die '
-            '{name}-Prüfung im Fach {subject} aus dem Jahr {year} in {state}.'
+            "Die Aufgaben, Erwartungshorizonte und Lösungen für die "
+            "{name}-Prüfung im Fach {subject} aus dem Jahr {year} in {state}."
         ).format(
-            subject=self.subject,
-            year=self.year,
-            name=name,
-            state=curriculum.state.name
+            subject=self.subject, year=self.year, name=name, state=curriculum.state.name
         )
         ref = self.get_reference(curriculum)
-        redirect_path = '/kampagnen/verschlusssache-pruefung/app/gesendet'
+        redirect_path = "/kampagnen/verschlusssache-pruefung/app/gesendet"
         query = {
-            'subject': subject.encode('utf-8'),
-            'body': body.encode('utf-8'),
-            'ref': ref.encode('utf-8'),
-            'redirect': redirect_path.encode('utf-8'),
+            "subject": subject.encode("utf-8"),
+            "body": body.encode("utf-8"),
+            "ref": ref.encode("utf-8"),
+            "redirect": redirect_path.encode("utf-8"),
         }
         hide_features = (
-            'hide_public', 'hide_full_text', 'hide_similar', 'hide_publicbody',
-            'hide_draft', 'hide_editing'
+            "hide_public",
+            "hide_full_text",
+            "hide_similar",
+            "hide_publicbody",
+            "hide_draft",
+            "hide_editing",
         )
-        query.update({f: b'1' for f in hide_features})
+        query.update({f: b"1" for f in hide_features})
         query = urlencode(query)
-        self._request_url = '%s?%s' % (url, query)
+        self._request_url = "%s?%s" % (url, query)
         return self._request_url
 
     def get_request_state(self):
         if self.request_pending:
-            return ('pending', 'Schon angefragt')
+            return ("pending", "Schon angefragt")
         elif self.request_failed:
-            return ('refused', 'Fehlgeschlagen')
+            return ("refused", "Fehlgeschlagen")
         elif self.request_successful:
-            return ('successful', 'Abgeschlossen')
+            return ("successful", "Abgeschlossen")
         else:
-            return ('request', 'Jetzt anfragen!')
+            return ("request", "Jetzt anfragen!")
 
     def display(self):
         if self.exam_request:
             if self.exam_request.url:
                 return {
-                    'link': self.exam_request.url,
-                    'title': 'Schon öffentlich',
-                    'state': 'successful',
-                    'icon': 'download'
+                    "link": self.exam_request.url,
+                    "title": "Schon öffentlich",
+                    "state": "successful",
+                    "icon": "download",
                 }
             elif not self.state.is_one_click:
                 state, title = self.get_request_state()
                 url = self.exam_request.foirequest.get_absolute_url()
-                return {
-                    'link': url + '#last',
-                    'state': state,
-                    'title': title
-                }
+                return {"link": url + "#last", "state": state, "title": title}
             elif not self.can_request:
                 state, title = self.get_request_state()
                 return {
-                    'link': self.get_same_request.get_absolute_url(),
-                    'state': state,
-                    'title': title
+                    "link": self.get_same_request.get_absolute_url(),
+                    "state": state,
+                    "title": title,
                 }
 
         if self.can_request:
